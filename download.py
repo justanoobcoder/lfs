fba625baacfcfead8cf35dcbeec74934451d8786
Nguyen Hong Hiep <syaorancode@gmail.com>
2026-02-04 02:52:45 +0000

    add scripts

---
+++ download.py
@@ -0,0 +1,64 @@
+'''
+* This script is for downloading, checking packages and patches of LFS.
+All failed packages, patches (mismatch md5sum) will be saved in
+failed_packages.csv and failed_patches.csv
+
+* Tested on Python3.9.9
+
+* Requirement:
+    - packages.csv and patches.csv from scrape.py script
+'''
+
+import csv
+import subprocess
+
+
+def download_file(name: str, url: str) -> tuple:
+    file_name = subprocess.check_output(
+        f'basename {url}', shell=True).decode().strip()
+    if subprocess.call(f'[ -f {file_name}  ]', shell=True) == 0:
+        return False, file_name
+    print(f'Downloading {name} from {url} ...')
+    subprocess.call(f'curl -L -s -O {url}', shell=True)
+    return True, file_name
+
+
+def check_file(file_name: str, name: str, md5sum: str) -> bool:
+    cmd_exit_code = subprocess.call(
+        f'echo "{md5sum} {file_name}"| md5sum -c > /dev/null', shell=True)
+    if cmd_exit_code != 0:
+        subprocess.call(f'rm -f {file_name}', shell=True)
+        print(name + ': md5sum mismatch!')
+        print(f'Deleted {file_name}')
+        return False
+    return True
+
+
+with open('failed_packages.csv', 'w', encoding='utf-8') as failed_packages_file:
+    failed_packages_file.write('')
+    csv_writer = csv.writer(failed_packages_file)
+    with open('packages.csv', 'r', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        for line in reader:
+            pkg_name = line[0]
+            pkg_version = line[1]
+            pkg_url = line[2]
+            pkg_md5sum = line[3]
+            is_successful, pkg_file_name = download_file(pkg_name, pkg_url)
+            if is_successful:
+                if not check_file(pkg_file_name, pkg_name, pkg_md5sum):
+                    csv_writer.writerow([pkg_name, pkg_version, pkg_url, pkg_md5sum])
+
+with open('failed_patches.csv', 'w', encoding='utf-8') as failed_patches_file:
+    failed_patches_file.write('')
+    csv_writer = csv.writer(failed_patches_file)
+    with open('patches.csv', 'r', encoding='utf-8') as file:
+        reader = csv.reader(file)
+        for line in reader:
+            pkg_name = line[0]
+            pkg_url = line[1]
+            pkg_md5sum = line[2]
+            is_successful, pkg_file_name = download_file(pkg_name, pkg_url)
+            if is_successful:
+                if not check_file(pkg_file_name, pkg_name, pkg_md5sum):
+                    csv_writer.writerow([pkg_name, pkg_url, pkg_md5sum])
---
+++ lfs.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+
+# LFS Version 12.4-systemd
+
+set -e
+
+# export LFS="/mnt/lfs"
+# export LC_ALL="POSIX"
+# export LFS_TGT="x86_64-lfs-linux-gnu"
+# export LFS_DISK="/dev/nvme0n1p7"
+# export SRC_DIR="$LFS/sources/src_dir"
+# export CONFIG_SITE="$LFS/usr/share/config.site"
+# export MAKEFLAGS="-j$(nproc)"
+
+error() { echo -e "$1" && exit 1; }
+
+echo -e "Make sure to mount $LFS_DISK before running this script!\nIf this is THE FIRST TIME you run this script, chown $LFS to your user!"
+
+select yn in "Continue" "Exit"; do
+    case $yn in
+    Continue)
+        break
+        ;;
+    Exit) error "User exits!" ;;
+    esac
+done
+
+if ! grep -q $LFS /proc/mounts; then
+    error "Mount $LFS_DISK to $LFS first!!!"
+fi
+
+echo "Running after 5 seconds ..."
+sleep 5
+
+mkdir -pv $LFS/{sources,etc,var} $LFS/usr/{bin,lib,sbin}
+mkdir -pv $SRC_DIR
+# if ! grep -q $SRC_DIR /proc/mounts; then
+#     sudo mount -t tmpfs -o size=10G tmpfs $SRC_DIR
+# fi
+[ -f $LFS/cleanedup ] || mkdir -pv $LFS/tools
+
+if ! [ -d $LFS/bin ]; then
+    for i in bin lib sbin; do
+        ln -sv usr/$i $LFS/$i
+    done
+fi
+
+case $(uname -m) in
+x86_64) mkdir -pv $LFS/lib64 ;;
+esac
+
+cp -rf *.csv *.sh *.py chapter* $LFS/sources
+
+cd $LFS/sources
+
+#export PATH="$LFS/tools/bin:$PATH"
+
+if [ ! -f packages.csv ] || [ ! -f patches.csv ]; then
+    if [ ! -d ./venv ]; then
+        python -m venv venv
+    fi
+    source ./venv/bin/activate
+    pip install -r requirements.txt
+    python scrape.py
+    deactivate
+fi
+python download.py
+
+if [ "$(cat failed_packages.csv | wc -l)" != "1" ] ||
+    [ "$(cat failed_patches.csv | wc -l)" != "1" ]; then
+    error "Some packages or patches are failed to download!"
+else
+    rm -f failed_patches.csv failed_packages.csv
+fi
+
+# Chapter 5
+#for package in binutils gcc linux-api-headers glibc libstdc++; do
+# for package in glibc libstdc++; do
+#     source install_package.sh 5 "$package"
+# done
+
+# Chapter 6
+for package in m4 ncurses bash coreutils diffutils file findutils \
+    gawk grep gzip make patch sed tar 'xz utils' binutils gcc; do
+    source install_package.sh 6 "$package"
+done
