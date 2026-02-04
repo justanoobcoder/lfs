8f91e290985c2ef24feea1fe1e31a4a71b77b1a8
Nguyen Hong Hiep <syaorancode@gmail.com>
2026-02-04 02:54:32 +0000

    done chapter 5

---
+++ chapter5/binutils.sh
@@ -0,0 +1,13 @@
+mkdir -v build
+cd       build
+
+../configure --prefix=$LFS/tools \
+             --with-sysroot=$LFS \
+             --target=$LFS_TGT   \
+             --disable-nls       \
+             --enable-gprofng=no \
+             --disable-werror    \
+             --enable-new-dtags  \
+             --enable-default-hash-style=gnu \
+&& make \
+&& make install
