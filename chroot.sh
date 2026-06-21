285e598acbebc6ecbb60d255951de10e5d27f2fa
Nguyen Hong Hiep <syaorancode@gmail.com>
2026-06-21 19:59:36 +0700

    add chroot.sh script

---
+++ chroot.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+LFS=/mnt/lfs
+
+chroot "$LFS" /usr/bin/env -i   \
+    HOME=/root                  \
+    TERM="$TERM"                \
+    PS1='(lfs chroot) \u:\w\$ ' \
+    PATH=/usr/bin:/usr/sbin     \
+    MAKEFLAGS="-j$(nproc)"      \
+    TESTSUITEFLAGS="-j$(nproc)" \
+    /bin/bash --login
+
+exit 0
